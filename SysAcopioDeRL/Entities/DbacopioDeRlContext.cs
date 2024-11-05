af29fb7bc763c221e80df34752bde97818b6def8
REMR11 <remr11@ufg.edu.sv>
2024-11-05 16:58:54 -0600

    Fix: Eliminando conexion incorrecta

--- SysAcopioDeRL/Entities/DbacopioDeRlContext.cs
+++ SysAcopioDeRL/Entities/DbacopioDeRlContext.cs
@@ -35,7 +35,6 @@ public partial class DbacopioDeRlContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        //=> optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=DBAcopioDeRL;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
         => optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("ConnectionString"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)

726272c80e408ff017e35ce1dcd24c7c7ac49aa8
REMR11 <remr11@ufg.edu.sv>
2024-11-05 21:38:48 -0600

    Feat: UnitTest

---
+++ SysAcopioDeRL.InfrastructureTests/Repositories/RecursosDonacionesRepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SysAcopioDeRL.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysAcopioDeRL.Infrastructure.Repositories.Tests
+{
+    [TestClass()]
+    public class RecursosDonacionesRepositoryTests
+    {
+        [TestMethod()]
+        public void GetByDonacionAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RecursosDonacionesRepositoryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByDonacionAsyncTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByRecursoAsyncTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

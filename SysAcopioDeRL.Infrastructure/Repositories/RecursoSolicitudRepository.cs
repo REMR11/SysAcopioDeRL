e1a0f46081f7e5ce93960b9796a55ce8a7b8115a
REMR11 <remr11@ufg.edu.sv>
2024-11-05 20:05:21 -0600

    Feat: Creacion de clase RecursoSolicitudRepository

---
+++ SysAcopioDeRL.Infrastructure/Repositories/RecursoSolicitudRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SysAcopioDeRL.Entities;
+using SysAcopioDeRL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SysAcopioDeRL.Infrastructure.Repositories
+{
+    public class RecursoSolicitudRepository : GenericRepository<RecursoSolicitud>, IRecursoSolicitudRepository
+    {
+        public RecursoSolicitudRepository(DbacopioDeRlContext pContext) : base(pContext)
+        {
+        }
+        /// <summary>
+        /// Metodo para obtener un listado de recursos por Id
+        /// </summary>
+        /// <param name="idRecurso"></param>
+        /// <returns> Listado de recursos que coincidan con el Id</returns>
+        public async Task<IEnumerable<RecursoSolicitud>> GetByRecursoAsync(long idRecurso)
+        {
+            return await dbContext.RecursoSolicituds
+                .Include(rs => rs.IdRecurso)
+                .Include(rs => rs.IdSolicitud)
+                .Where(rs => rs.IdRecurso == idRecurso)
+                .ToListAsync();
+
+        }
+
+        /// <summary>
+        /// Metodo para obtener un listado de solicitudes por Id
+        /// </summary>
+        /// <param name="idSolicitud"></param>
+        /// <returns> Listado de solicitudes que coinciden con el Id</returns>
+        public async Task<IEnumerable<RecursoSolicitud>> GetBySolicitudAsync(long idSolicitud)
+        {
+            return await dbContext.RecursoSolicituds
+                .Include(rs => rs.IdRecurso)
+                .Include(rs => rs.IdSolicitud)
+                .Where(rs => rs.IdSolicitud == idSolicitud)
+                .ToListAsync();
+
+        }
+    }
+}
